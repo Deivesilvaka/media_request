014a5b9903aaf410cda29838ea99dddb82564d6d
Luiz Davi <luizdaviinformatica@gmail.com>
2021-03-25 11:07:19 -0300

    adição da pagina de teste

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 
 const routes = require('express').Router()
 const controller = require('./src/controller/controller')
+const {join} = require('path')
 
 routes.get('/single', controller.getSingleMedia)
 routes.get('/media', controller.getMedia)
 routes.get('/download', controller.download)
+routes.get('/', (req, res) => res.sendFile(join(__dirname, "index.html")))
 
 module.exports = routes
